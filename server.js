782de12023c70aec87b9b0775d9e533293d9f367
Tomás De Los Santos <delossantostomas12@gmail.com>
2022-07-26 22:23:52 -0300

    Admin Check

--- server.js
+++ server.js
@@ -10,15 +10,20 @@ const PORT = process.env.PORT || 8080;
 const data = new fileSystem('./products.txt');
 const cart = new fileSystem('./cart.txt');
 
+let admin = true;
+
+const adminCheck = (req, res, next) => {
+    if(admin == true){
+        next();
+    } else{
+        res.send({ error: -1, description: `Route not authorized`})
+    }
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/public'));
-
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('../client/build'))
-// } else{
-// }
 
 // app.use('/api', router);
 app.use('/api/products', productRouter);
@@ -30,7 +35,7 @@ productRouter.get('/', async (req,res) => {
     res.json(products); 
 })
 
-productRouter.post('/', async (req,res) => {
+productRouter.post('/', adminCheck, async (req,res) => {
     try {
         data.save(req.body);
         res.json(req.body.title);
@@ -40,7 +45,7 @@ productRouter.post('/', async (req,res) => {
     }
 })
 
-productRouter.put('/:id', async (req, res) => {
+productRouter.put('/:id', adminCheck, async (req, res) => {
     let id = req.params.id;
     let body = req.body;
     try {
@@ -51,7 +56,7 @@ productRouter.put('/:id', async (req, res) => {
     }
 })
 
-productRouter.delete('/:id', async (req, res) => {
+productRouter.delete('/:id', adminCheck, async (req, res) => {
     // let products = await data.getAll();
     let book = await data.getById(req.params.id);
     if(book){
